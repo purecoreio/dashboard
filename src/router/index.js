809c73d6e8d1b52b9b5213935c7bf508d4c0bb9f
quiquelhappy <1636461@uab.cat>
2022-04-21 17:06:12 +0200

    network and login routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,59 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    redirect: "/network"
+  },
+  {
     path: '/login',
     name: 'login',
-    component: function () {
+    component: () => {
       return import('../views/Login.vue')
     }
-  }
+  },
+  {
+    path: '/network',
+    name: 'noNetwork',
+    component: () => {
+      return import('../views/network/unselected/Unselected.vue')
+    },
+    children: [
+      {
+        path: "",
+        component: () => {
+          return import('../views/network/unselected/Select.vue')
+        }
+      },
+      {
+        path: "create",
+        component: () => {
+          return import('../views/network/unselected/Create.vue')
+        }
+      },
+    ]
+  },
+  {
+    path: '/network/:id',
+    name: 'network',
+    component: () => {
+      return import('../views/network/selected/Network.vue')
+    },
+    redirect: (to) => { return `/network/${to.params.id}/instance` },
+    children: [
+      {
+        path: "instance",
+        component: () => {
+          return import('../views/network/selected/instance/Instance.vue')
+        }
+      },
+      {
+        path: "settings",
+        component: () => {
+          return import('../views/network/selected/settings/Settings.vue')
+        }
+      },
+    ]
+  },
 ]
 
 const router = createRouter({
