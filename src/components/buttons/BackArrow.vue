598935c472e4acd9f04b4aaf8ee97516b109f448
quiquelhappy <1636461@uab.cat>
2022-04-22 18:21:50 +0200

    support for synethic back redirect

--- src/components/buttons/BackArrow.vue
+++ src/components/buttons/BackArrow.vue
@@ -1,7 +1,12 @@
 <template>
-  <router-link class="back-arrow" :to="path">
-    <v-icon icon="mdi-keyboard-backspace" />
-  </router-link>
+  <div style="display: inline-block">
+    <router-link v-if="to" class="back-arrow" :to="to">
+      <v-icon icon="mdi-keyboard-backspace" />
+    </router-link>
+    <a v-if="!to" @click="clicked" class="back-arrow"
+      ><v-icon icon="mdi-keyboard-backspace"
+    /></a>
+  </div>
 </template>
 <style scoped>
 .back-arrow {
@@ -9,15 +14,20 @@
   color: gray;
   text-decoration: none;
 }
-.back-arrow:hover{
+.back-arrow:hover {
   color: rgb(219, 219, 219);
 }
-.back-arrow:active{
+.back-arrow:active {
   color: white;
 }
 </style>
 <script>
 export default {
-  props: ["path"],
+  props: ["to"],
+  methods: {
+    clicked() {
+      this.$emit("clicked");
+    },
+  },
 };
 </script>
