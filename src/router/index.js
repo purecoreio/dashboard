9b4e1bf73b4c295b99542dcc43f49b14f0dab051
quiquelhappy <1636461@uab.cat>
2022-04-22 18:24:57 +0200

    add missing routes

--- src/router/index.js
+++ src/router/index.js
@@ -48,9 +48,91 @@ const routes = [
         }
       },
       {
+        path: "instance/:instance",
+        component: () => {
+          return import('../views/network/selected/Wrapper.vue')
+        },
+        redirect: (to) => { return `/network/${to.params.id}/instance/${to.params.instance}/settings` },
+        children: [
+          {
+            path: "settings",
+            component: () => {
+              return import('../views/network/selected/instance/view/Settings.vue')
+            }
+          },
+          {
+            path: "players",
+            component: () => {
+              return import('../views/network/selected/instance/view/Players.vue')
+            }
+          },
+          {
+            path: "performance",
+            component: () => {
+              return import('../views/network/selected/instance/view/Performance.vue')
+            }
+          },
+          {
+            path: "console",
+            component: () => {
+              return import('../views/network/selected/instance/view/Console.vue')
+            }
+          },
+        ]
+      },
+      {
         path: "settings",
         component: () => {
-          return import('../views/network/selected/settings/Settings.vue')
+          return import('../views/network/selected/Wrapper.vue')
+        },
+        redirect: (to) => { return `/network/${to.params.id}/settings/identity` },
+        children: [
+          {
+            path: "identity",
+            name: "Identity",
+            component: () => {
+              return import('../views/network/selected/settings/Identity.vue')
+            }
+          },
+          {
+            path: "dangerzone",
+            name: "Danger Zone",
+            component: () => {
+              return import('../views/network/selected/settings/DangerZone.vue')
+            }
+          },
+        ]
+      },
+      {
+        path: "store",
+        component: () => {
+          return import('../views/network/selected/Wrapper.vue')
+        },
+        redirect: (to) => { return `/network/${to.params.id}/store/package` },
+        children: [
+          {
+            path: "package",
+            name: "packages",
+            meta: {
+              beta: true
+            },
+            component: () => {
+              return import('../views/network/selected/store/Package.vue')
+            }
+          },
+          {
+            path: "gateway",
+            name: "gateways",
+            component: () => {
+              return import('../views/network/selected/store/Gateway.vue')
+            }
+          },
+        ]
+      },
+      {
+        path: "/:catchAll(.*)",
+        component: () => {
+          return import('../views/errors/UnderDevelopment.vue')
         }
       },
     ]
