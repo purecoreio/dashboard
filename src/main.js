f84bddd5eeb118e51a105b855a0b366a077c4991
quiquelhappy <quiquelhappy@gmail.com>
2022-06-28 18:24:23 +0200

    google analytics

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import VueSmoothScroll from 'v-smooth-scroll'
+import VueGtag from "vue-gtag";
 
 import purecore from "purecore";
 
 const app = createApp(App)
 
-app.config.globalProperties.purecore = new purecore(undefined, import.meta.env.MODE == 'lldev')
+const mode = import.meta.env.MODE
+app.config.globalProperties.purecore = new purecore(undefined, mode == 'lldev')
 app.config.globalProperties.context = {}
 
 app.use(router)
@@ -19,4 +21,17 @@ app.use(router)
     updateHistory: false,
     easingFunction: t => t * (2 - t),
   })
-  .mount('#app')
+
+if (!String(mode).includes("dev")) {
+  app.use(VueGtag, {
+    config: {
+      id: "G-VJ3VLD1HH2",
+      params: {
+        anonymize_ip: true
+      }
+    }
+  }, router)
+} else {
+  console.log("analytics disabled on dev mode")
+}
+app.mount('#app')
